5e63a412a6f4fd5647a408f38cdf2bc6b3349b7b
elninocavalcanti10 <acn2011@outlook.com>
2022-03-21 14:15:53 -0300

    Ajustando o logo

--- src/components/HeaderSite/index.tsx
+++ src/components/HeaderSite/index.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import { HeaderLogo } from '../../styles/components/HeaderSite';
 
 export default function HeaderSite() {
     return (
         <HeaderLogo>
-            <Image src="/logo.png" alt="logo" height="30px" width="250px" />
+            <img src="/logo.png" alt="logo" />
         </HeaderLogo>
     )
 }
--- src/styles/components/HeaderSite.ts
+++ src/styles/components/HeaderSite.ts
@@ -9,18 +9,15 @@ export const HeaderLogo = styled.div`
     align-items:center;
     img {
         width: 50%;
-        height: 100%;
     }
     @media(max-width: 900px) {
         img {
             width: 75%;
-            height: 100%;
         }
     }
     @media(max-width: 600px) {
         img {
             width: 100%;
-            height: 100%;
         }
     }
 `;
